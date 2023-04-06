074e1ac2bf27393df3ed1e3244f3f4b1c7dbdaf5
francescolonardo <francescolonardo@gmail.com>
2023-04-06 10:32:56 +0200

    Updated scripts

--- gatherer_writeups.py
+++ gatherer_writeups.py
@@ -7,27 +7,22 @@ import secrets
 
 import openai
 
-
-MODEL_ID = "gpt-3.5-turbo"
-# more contextually relevant and less creative responses (from 0.0 to 2.0)
-RANDOMNESS_LEVEL = 0.5
-WRITEUP_CMD = "CMD: new_writeup"
-NONCE_LEN = 21
-WRITEUPS_DIRNAME = "./dataset_writeups/"
-TXT_SUFFIXES = ["_original.txt", "_cleaned.txt", "_tagged.txt"]
+from settings import MODEL_ID, MODEL_RANDOMNESS_GATHERER
+from settings import DATASET_DIRPATH, DATASET_TXT_SUFFIXES
+from settings import NEW_WRITEUP_CMD, NONCE_LENGTH
 
 
 def get_response(messages: list):
     response = openai.ChatCompletion.create(  # https://platform.openai.com/docs/api-reference/chat/create
         model=MODEL_ID,
         messages=messages,
-        temperature=RANDOMNESS_LEVEL,
+        temperature=MODEL_RANDOMNESS_GATHERER,
         stream=False,
     )
     return response.choices[0].message
 
 
-def generate_nonce(length=NONCE_LEN):
+def generate_nonce(length=NONCE_LENGTH):
     # generate a random string of uppercase letters and digits
     alphabet = string.ascii_uppercase + string.digits
     return ''.join(secrets.choice(alphabet) for _ in range(length))
@@ -44,25 +39,26 @@ def clean_writeup(file_content):
 
 
 def write_file(out_filepath, file_content):
-    with open(out_filepath, "w") as output_file:
+    with open(out_filepath, "w", encoding="utf-8") as output_file:
         output_file.write(file_content)
 
 
 if __name__ == "__main__":
     # create an argument for the integer parameter
     parser = argparse.ArgumentParser()
-    parser.add_argument("-n", type=int,
+    parser.add_argument("-n", dest="number", type=int,
                         help="the number of writeups to gather")
     args = parser.parse_args()
-    num_writeups = args.n
+    # get the number of writeups to scan
+    num_writeups = args.number
     # check if the integer parameter is provided
     if not num_writeups:
         parser.error("an integer parameter is required")
 
-    with open("./api_keys.json") as f:
+    with open("./api_keys.json", "r", encoding="utf-8") as f:
         api_key = json.load(f)["api_key"]
     openai.api_key = api_key
-    with open("./guidelines.json") as f:
+    with open("./guidelines.json", "r", encoding="utf-8") as f:
         requirements = json.load(f)
         basic_requirements = requirements["basic_requirements"]
         gatherer_requirements = requirements["gatherer_requirements"]
@@ -70,50 +66,54 @@ if __name__ == "__main__":
     print("Sending the context...")
     get_response(messages=guidelines)
 
-    print("Sending the writeups history...")
-    # loop through all directories and subdirectories
-    for dirpath, dirnames, filenames in os.walk(WRITEUPS_DIRNAME):
-        # loop through all files in the directory
-        for filename_idx, filename in enumerate(filenames, start=1):
-            # check if the file is a "*_original.txt" file
-            if filename.endswith(TXT_SUFFIXES[0]):
-                prefix_filename = filename.split(TXT_SUFFIXES[0])[0]
-                if os.path.exists(os.path.join(WRITEUPS_DIRNAME, prefix_filename+TXT_SUFFIXES[2])):
-                    with open(os.path.join(WRITEUPS_DIRNAME, prefix_filename+TXT_SUFFIXES[2]), "r") as file:
-                        # take the first five lines of a tagged writeup
-                        file_summary = [line for i,
-                                        line in enumerate(file) if i < 5]
-                        file_summary = "".join(file_summary)
-                    guidelines.append(
-                        {"role": "user", "content": file_summary})
-                    # five writeups unwanted at a time
-                    if filename_idx % 5 == 0 or filename_idx == len(filenames):
-                        get_response(messages=guidelines)
+    try:
+        print("Sending the writeups history...")
+        # loop through all directories and subdirectories
+        for dirpath, dirnames, filenames in os.walk(DATASET_DIRPATH):
+            # loop through all files in the directory
+            for filename_idx, filename in enumerate(filenames, start=1):
+                # check if the file is a "*_original.txt" file
+                if filename.endswith(DATASET_TXT_SUFFIXES[0]):
+                    prefix_filename = filename.split(DATASET_TXT_SUFFIXES[0])[0]
+                    if os.path.exists(os.path.join(DATASET_DIRPATH, prefix_filename+DATASET_TXT_SUFFIXES[2])):
+                        with open(os.path.join(DATASET_DIRPATH, prefix_filename+DATASET_TXT_SUFFIXES[2]), "r", encoding="utf-8") as file:
+                            # take the first five lines of a tagged writeup
+                            file_summary = [line for i,
+                                            line in enumerate(file) if i < 5]
+                            file_summary = "".join(file_summary)
+                        guidelines.append(
+                            {"role": "user", "content": file_summary})
+                        # five writeups unwanted at a time
+                        if filename_idx % 5 == 0 or filename_idx == len(filenames):
+                            get_response(messages=guidelines)
 
-    print("Gathering new writeups...")
-    for writeup_idx in range(num_writeups):
-        guidelines.append({"role": "user", "content": WRITEUP_CMD})
-        max_retries = 3
-        retry_count = 0
-        while retry_count < max_retries:
-            try:
-                assistant_output = get_response(messages=guidelines)
-                guidelines.append(assistant_output)
-                # clean writeup content
-                file_content = clean_writeup(assistant_output["content"])
-                # generate a nonce
-                filename = f"writeup_{generate_nonce()}.txt"
-                if not os.path.exists(WRITEUPS_DIRNAME):
-                    os.makedirs(WRITEUPS_DIRNAME)
-                out_filepath = os.path.join(WRITEUPS_DIRNAME, filename)
-                write_file(out_filepath, file_content)
-                print(f"{writeup_idx+1}. Writeup file: {out_filepath}")
-                break  # exit the loop if the function call succeeds
-            except openai.error.InvalidRequestError as e:
-                print(f"OpenAI API request is invalid: {e}")
-                retry_count += 1
-                if retry_count == max_retries:
-                    break
-            except openai.error.RateLimitError as e:
-                print(f"OpenAI API request exceeds rate limit: {e}")
-                time.sleep(60)  # sleep for 60 seconds
+        print("Gathering new writeups...")
+        for writeup_idx in range(num_writeups):
+            guidelines.append({"role": "user", "content": NEW_WRITEUP_CMD})
+            max_retries = 5
+            retry_count = 0
+            while retry_count < max_retries:
+                try:
+                    assistant_output = get_response(messages=guidelines)
+                    guidelines.append(assistant_output)
+                    # clean writeup content
+                    file_content = clean_writeup(assistant_output["content"])
+                    # generate a nonce
+                    filename = f"writeup_{generate_nonce()}.txt"
+                    if not os.path.exists(DATASET_DIRPATH):
+                        os.makedirs(DATASET_DIRPATH)
+                    out_filepath = os.path.join(DATASET_DIRPATH, filename)
+                    write_file(out_filepath, file_content)
+                    print(f"{writeup_idx+1}. Writeup file: {out_filepath}")
+                    break  # exit the loop if the function call succeeds
+                except openai.error.InvalidRequestError as e:
+                    print(f"OpenAI API request is invalid: {e}")
+                    retry_count += 1
+                    if retry_count == max_retries:
+                        break
+                except openai.error.RateLimitError as e:
+                    print(f"OpenAI API request exceeds rate limit: {e}")
+                    time.sleep(60)  # sleep for 60 seconds
+        print("Done.")
+    except KeyboardInterrupt:
+        print("Stop.")
--- tagger_writeups.py
+++ tagger_writeups.py
@@ -2,26 +2,22 @@ import json
 import os
 import re
 import time
+import argparse
+import unicodedata
 
 import openai
+import chardet
 
-
-MODEL_ID = "gpt-3.5-turbo"
-# more contextually relevant and less creative responses (from 0.0 to 2.0)
-RANDOMNESS_LEVEL = 0.2
-WRITEUPS_DIRPATH = "./dataset_writeups/"
-HUMANS_TAGGED_DIRNAME = "humans"
-TXT_SUFFIXES = ["_original.txt", "_cleaned.txt", "_tagged.txt"]
-LABELS_TO_REMOVE = ["# overview", "# analysis",
-                    "# attack execution", "# attack_execution"]
-REQUESTED_LABELS = ["#overview", "#analysis", "#attack_execution"]
+from settings import OLD_LABELS, NEW_LABELS
+from settings import MODEL_ID, MODEL_RANDOMNESS_TAGGER
+from settings import DATASET_DIRPATH, HUMANS_TAGGED_DIRNAME, DATASET_TXT_SUFFIXES
 
 
 def get_response(messages: list):
     response = openai.ChatCompletion.create(  # https://platform.openai.com/docs/api-reference/chat/create
         model=MODEL_ID,
         messages=messages,
-        temperature=RANDOMNESS_LEVEL,
+        temperature=MODEL_RANDOMNESS_TAGGER,
         stream=False,
     )
     return response.choices[0].message
@@ -31,11 +27,27 @@ def search_strings_in_text(text, strings_list):
     return any(s in text for s in strings_list)
 
 
+def normalize_file(in_filepath):
+    # open the file in binary mode and read its content
+    with open(in_filepath, "rb") as input_f:
+        content = input_f.read()
+    # detect the file's encoding
+    encoding = chardet.detect(content)["encoding"]
+    # decode the contents of the file using the detected encoding
+    decoded_content = content.decode(encoding)
+    # normalize the text by replacing non-ASCII characters with their closest ASCII equivalent
+    normalized_content = unicodedata.normalize(
+        'NFKD', decoded_content).encode("ascii", "ignore").decode("utf-8")
+    # write the normalized contents back to the file
+    with open(in_filepath, "wb") as output_f:
+        output_f.write(normalized_content.encode("utf-8"))
+
+
 def read_clean_file(in_filepath):
-    with open(in_filepath, "r") as input_f:
+    with open(in_filepath, "r", encoding="utf-8") as input_f:
         file_content = input_f.read()
         # remove the specified strings
-        for string in LABELS_TO_REMOVE:
+        for string in OLD_LABELS:
             pattern = re.compile(re.escape(string), re.IGNORECASE)
             file_content = pattern.sub("", file_content)
         # remove empty lines # TODO: maybe unneeded
@@ -72,10 +84,23 @@ class NotCorrectlyTagged(Exception):
 
 
 if __name__ == "__main__":
-    with open("./api_keys.json") as f:
+    # create an argument for the string parameter 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-dir', dest='directory', default=DATASET_DIRPATH,
+                        help='the directory to scan for tagging')
+    # create an argument for the integer parameter
+    parser.add_argument("-n", dest="number", type=int, default=10000,
+                        help="the number of writeups to tag")
+    args = parser.parse_args()
+    # get the directory to scan
+    starting_directory = args.directory
+    # get the number of writeups to scan
+    num_writeups = args.number
+
+    with open("./api_keys.json", "r", encoding="utf-8") as f:
         api_key = json.load(f)["api_key"]
     openai.api_key = api_key
-    with open("./guidelines.json") as f:
+    with open("./guidelines.json", "r", encoding="utf-8") as f:
         requirements = json.load(f)
         basic_requirements = requirements["basic_requirements"]
         tagger_requirements = requirements["tagger_requirements"]
@@ -83,84 +108,95 @@ if __name__ == "__main__":
     print("Sending the context...")
     get_response(messages=guidelines)
 
-    print("Tagging new writeups...")
-    # loop through all directories and subdirectories
-    for dirpath, dirnames, filenames in os.walk(WRITEUPS_DIRPATH):
-        # loop through all files in the directory
-        # skip the human tagged writeups
-        if HUMANS_TAGGED_DIRNAME in dirnames:
-            dirnames.remove(HUMANS_TAGGED_DIRNAME)
-        for filename in filenames:
-            # check if the file is a text file
-            if filename.endswith(".txt"):
-                if not filename.endswith(TXT_SUFFIXES[2]):
-                    # check that the file is not yet tagged
-                    prefix_filename = filename.split(".")[0]
-                    if not os.path.exists(os.path.join(dirpath, prefix_filename+TXT_SUFFIXES[2])):
-                        # original file
-                        in_filepath = os.path.join(
-                            dirpath, filename)
-                        print(f"Writeup file (original): {in_filepath}")
-                        # cleaned file
-                        file_content = read_clean_file(in_filepath)
-                        # TODO: remove this (?)
-                        '''
-                        out_filepath = os.path.splitext(
-                            in_filepath)[0] + TXT_SUFFIXES[1].split(".")[0] + os.path.splitext(in_filepath)[1]
-                        write_file(out_filepath, file_content)
-                        #print(f"Writeup file (cleaned): {out_filepath}")
-                        '''
-                        guidelines.append(
-                            {"role": "user", "content": file_content})  # add the writeup
-                        max_retries = 10
-                        retry_count = 0
-                        while retry_count < max_retries:
-                            try:
-                                # tagged file
-                                assistant_output = get_response(
-                                    messages=guidelines)
-                                # check if the response is correctly tagged
-                                if not search_strings_in_text(assistant_output["content"], REQUESTED_LABELS):
-                                    raise NotCorrectlyTagged(
-                                        "Writeup not correctly tagged. Retrying...")
-                                # guidelines.append(assistant_output)
-                                out_filepath = os.path.splitext(
-                                    in_filepath)[0] + TXT_SUFFIXES[2].split(".")[0] + os.path.splitext(in_filepath)[1]
-                                write_file(
-                                    out_filepath, assistant_output["content"])
-                                print(f"Writeup file (tagged): {out_filepath}")
-                                # TODO: remove this (?)
-                                '''
-                                # rename original file
-                                new_filename = filename.replace(
-                                    ".txt", TXT_SUFFIXES[0])
-                                os.rename(os.path.join(dirpath, filename),
-                                        os.path.join(dirpath, new_filename))
-                                '''
-                                guidelines.pop()  # remove the writeup
-                                break  # exit the loop if the function call succeeds
-                            except NotCorrectlyTagged as e:
-                                print(e.message)
-                                retry_count += 1
-                            except openai.error.InvalidRequestError as e:
-                                if str(e).startswith("This model's maximum context length"):
+    try:
+        print("Tagging new writeups...")
+        # loop through all directories and subdirectories
+        for dirpath, dirnames, filenames in os.walk(starting_directory):
+            # loop through all files in the directory
+            # skip the human tagged writeups
+            if HUMANS_TAGGED_DIRNAME in dirnames:
+                dirnames.remove(HUMANS_TAGGED_DIRNAME)
+            for filename in filenames:
+                # check if the file is a text file
+                if filename.endswith(".txt"):
+                    if not filename.endswith(DATASET_TXT_SUFFIXES[2]):
+                        # check that the file is not yet tagged
+                        prefix_filename = filename.split(".")[0]
+                        if not os.path.exists(os.path.join(dirpath, prefix_filename+DATASET_TXT_SUFFIXES[2])):
+                            num_writeups = num_writeups - 1
+                            # original file
+                            in_filepath = os.path.join(
+                                dirpath, filename)
+                            print(f"Writeup file (original): {in_filepath}")
+                            # normalized file
+                            file_content = normalize_file(in_filepath)
+                            # cleaned file
+                            file_content = read_clean_file(in_filepath)
+                            # TODO: remove this (?)
+                            '''
+                            out_filepath = os.path.splitext(
+                                in_filepath)[0] + TXT_SUFFIXES[1].split(".")[0] + os.path.splitext(in_filepath)[1]
+                            write_file(out_filepath, file_content)
+                            #print(f"Writeup file (cleaned): {out_filepath}")
+                            '''
+                            guidelines.append(
+                                {"role": "user", "content": file_content})  # add the writeup
+                            max_retries = 10
+                            retry_count = 0
+                            while retry_count < max_retries:
+                                try:
+                                    # tagged file
+                                    assistant_output = get_response(
+                                        messages=guidelines)
+                                    # check if the response is correctly tagged
+                                    if not search_strings_in_text(assistant_output["content"], NEW_LABELS):
+                                        raise NotCorrectlyTagged(
+                                            "Writeup not correctly tagged. Retrying...")
+                                    # guidelines.append(assistant_output)
+                                    out_filepath = os.path.splitext(
+                                        in_filepath)[0] + DATASET_TXT_SUFFIXES[2].split(".")[0] + os.path.splitext(in_filepath)[1]
+                                    write_file(
+                                        out_filepath, assistant_output["content"])
                                     print(
-                                        "OpenAI API request exceeds maximum context length. Retrying...")
-                                else:
-                                    # TODO: remove this
-                                    print(f"{e}\n FIX THIS ERROR!")
-                                retry_count += 1
-                            except openai.error.RateLimitError as e:
-                                print(
-                                    "OpenAI API request exceeds rate limit. Slowing down...")
-                                time.sleep(60)  # sleep for 60 seconds
-                            finally:
-                                if retry_count == max_retries:
-                                    # delete files
+                                        f"Writeup file (tagged): {out_filepath}")
+                                    # TODO: remove this (?)
                                     '''
-                                    delete_file(filepath=out_filepath)
-                                    delete_file(filepath=os.path.join(
-                                        dirpath, filename))
+                                    # rename original file
+                                    new_filename = filename.replace(
+                                        ".txt", TXT_SUFFIXES[0])
+                                    os.rename(os.path.join(dirpath, filename),
+                                            os.path.join(dirpath, new_filename))
                                     '''
-                                    break
-    print("Done.")
+                                    break  # exit the loop if the function call succeeds
+                                except NotCorrectlyTagged as e:
+                                    print(e.message)
+                                    retry_count += 1
+                                except openai.error.InvalidRequestError as e:
+                                    if str(e).startswith("This model's maximum context length"):
+                                        print(
+                                            "OpenAI API request exceeds maximum context length. Retrying...")
+                                    else:
+                                        # TODO: remove this
+                                        print(f"{e}\n FIX THIS ERROR!")
+                                    retry_count += 1
+                                except openai.error.RateLimitError as e:
+                                    print(
+                                        "OpenAI API request exceeds rate limit. Slowing down...")
+                                    time.sleep(60)  # sleep for 60 seconds
+                                finally:
+                                    if retry_count == max_retries:
+                                        # delete files
+                                        '''
+                                        delete_file(filepath=out_filepath)
+                                        delete_file(filepath=os.path.join(
+                                            dirpath, filename))
+                                        '''
+                                        break
+                            guidelines.pop()  # remove the writeup
+                            if num_writeups == 0:
+                                break
+                if num_writeups == 0:
+                    break
+        print("Done.")
+    except KeyboardInterrupt:
+        print("Stop.")
